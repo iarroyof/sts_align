a5325f960d8327628c88f2c8517ef5a619097433
iarroyof <axayakac@gmail.com>
2017-05-30 19:20:57 -0500

    last tabulator version

--- tabulate_results.sh
+++ tabulate_results.sh
@@ -4,4 +4,10 @@ c=$3
 
 printf "Configuration Cosine Euclidean Manhattan\n" > all_results
 paste <(for r in `ls "$pair".output*`; do printf "%s\\t%s\\t%f\\n" "${r##*output_}" $(perl $NLP/correlation-noconfidence.pl "$gs" <(awk -F '\\t' '{print match($1, /[^ ]/) ? $1 : "0.1"}' "$r")); done) <(for r in `ls "$pair".output*`; do printf "%s\\t%s\\t%f\\n" "${r##*output_}" $(perl $NLP/correlation-noconfidence.pl "$gs" <(awk -F '\\t' '{print match($2, /[^ ]/) ? $2 : "0.1"}' "$r")); done) <(for r in `ls "$pair".output*`; do printf "%s\\t%s\\t%f\\n" "${r##*output_}" $(perl $NLP/correlation-noconfidence.pl "$gs" <(awk -F '\\t' '{print match($3, /[^ ]/) ? $3 : "0.1"}' "$r")); done) | awk '{ print $1, $3, $6, $9 }' >> all_results
-cat all_results|sed 's/^-\(.*\)/\1-/' | sort -g|sort -gk "$c"
+#cat all_results|sed 's/^-\(.*\)/\1-/' | sort -g|sort -gk "$c"
+
+if [ ! -z $3 ]; then
+    sort -nrk "$c" all_results | head -n20
+else
+    head all_results
+fi
